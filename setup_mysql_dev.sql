930494a5805fd33f8d707bff34b41d93b719e4a9
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-22 19:30:36 +0100

    commenting

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,8 @@
+-- Commenting
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
-USE mysql; -- Switch to the mysql system database
+USE mysql;
 
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
 
