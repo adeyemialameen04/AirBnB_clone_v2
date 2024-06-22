c6a4743e57954680dd7fab81f891c04552004d5b
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-22 19:27:41 +0100

    grant option

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,6 +3,6 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 USE mysql;
 
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
 
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
