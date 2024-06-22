f5414ce0b16f6da00bd7932d61af083376e66f85
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-22 19:26:30 +0100

    use

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,8 @@
--- creating a dabase setup
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
-USE mysql; -- Switch to the mysql system database
+USE mysql;
 
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
