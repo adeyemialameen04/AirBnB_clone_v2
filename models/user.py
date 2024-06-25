48654ea2497328f958969446273d86faec8950b9
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-25 17:28:55 +0100

    fix 3

--- models/user.py
+++ models/user.py
@@ -2,15 +2,19 @@
 """Documenting the user model"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from envs import HBNB_TYPE_STORAGE
 
 
 class User(BaseModel, Base):
     """The user class"""
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    # password = ""
-    # first_name = ""
-    # last_name = ""
+    if HBNB_TYPE_STORAGE == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
