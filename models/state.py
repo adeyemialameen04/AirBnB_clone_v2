2a9c9600d064e3d6e5abebb06fd3cd74d2a305e0
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-25 19:08:58 +0100

    hei God

--- models/state.py
+++ models/state.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from envs import HBNB_TYPE_STORAGE
-from models.city import City
-from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from os import getenv
 
 
 class State(BaseModel, Base):
-    """State docs"""
-    __tablename__ = 'states'
-    if HBNB_TYPE_STORAGE == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state',
-                              cascade='all, delete, delete-orphan')
-    else:
-        name = ''
+    """ State class """
 
+    __tablename__ = "states"
+    name = Column("name", String(128), nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        """Intilizes states"""
+        super().__init__(*args, **kwargs)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
+        # id = Column(String(60), primary_key=True, nullable=False)
+    else:
         @property
         def cities(self):
-            """Joined cities"""
-            from models import storage
-            joined_cities = []
-            cities = storage.all(City)
-            for city in cities.values():
+            import models
+            from models.city import City
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
                 if city.state_id == self.id:
-                    joined_cities.append(city)
-            return joined_cities
+                    city_list.append(city)
+            return city_list
+            # if value.to_dict()["state_id"] == self.id:
