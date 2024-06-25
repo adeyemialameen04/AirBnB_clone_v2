da7f46fd117d23e5a1aaf6d57677e0eb7dd61548
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-25 20:02:16 +0100

    no check

--- models/user.py
+++ models/user.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 """Documenting the user model"""
+from envs import HBNB_TYPE_STORAGE
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+    """The user class"""
+    __tablename__ = "users"
+    # if HBNB_TYPE_STORAGE == "db":
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship("Place", backref="user", cascade="all, delete")
-    reviews = relationship("Review", backref="user", cascade="all, delete")
+    # else:
+    #     email = ""
+    #     password = ""
+    #     first_name = ""
+    #     last_name = ""
