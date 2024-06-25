d0c59471766afb78dc61f4c02ba4123068cb810e
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-25 17:22:30 +0100

    user fix

--- models/user.py
+++ models/user.py
@@ -7,11 +7,10 @@ from sqlalchemy import Column, String
 class User(BaseModel, Base):
     """The user class"""
     __tablename__ = "users"
-    email = Column("email", String(128), nullable=False)
-    password = Column("password", String(128), nullable=False)
-    first_name = Column("first_name", String(128), nullable=False)
-    last_name = Column("last_name", String(128), nullable=False)
-    # email = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)"
     # password = ""
     # first_name = ""
     # last_name = ""
