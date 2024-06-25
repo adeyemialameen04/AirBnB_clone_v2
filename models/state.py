7a58ecdc46a6d4e401364917a3719a91b9487f59
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-25 19:57:14 +0100

    reoved

--- models/state.py
+++ models/state.py
@@ -1,34 +1,12 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""Documenting the user model"""
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
-from os import getenv
+from sqlalchemy import Column, String, ForeignKey
 
 
 class State(BaseModel, Base):
-    """ State class """
-
+    """The state class"""
     __tablename__ = "states"
     name = Column("name", String(128), nullable=False)
 
-    def __init__(self, *args, **kwargs):
-        """Intilizes states"""
-        super().__init__(*args, **kwargs)
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state",
-                              cascade="all, delete-orphan")
-        # id = Column(String(60), primary_key=True, nullable=False)
-    else:
-        @property
-        def cities(self):
-            import models
-            from models.city import City
-            city_list = []
-            all_cities = models.storage.all(City)
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
-            # if value.to_dict()["state_id"] == self.id:
+    # name = ""
--- models/user.py
+++ models/user.py
@@ -3,23 +3,18 @@
 from envs import HBNB_TYPE_STORAGE
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """The user class"""
     __tablename__ = "users"
-    if HBNB_TYPE_STORAGE == "db":
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        # places = relationship('Place', backref='user',
-        #                       cascade='all, delete, delete-orphan')
-        # reviews = relationship('Review', backref='user',
-        #                        cascade='all, delete, delete-orphan')
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    # if HBNB_TYPE_STORAGE == "db":
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    # else:
+    #     email = ""
+    #     password = ""
+    #     first_name = ""
+    #     last_name = ""
