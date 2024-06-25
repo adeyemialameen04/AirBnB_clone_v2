82cd9d6d276c940ac2b696c027bef8cced232aa4
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-25 16:34:47 +0100

    user

--- models/user.py
+++ models/user.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 """Documenting the user model"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """The user class"""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = "users"
+    email = Column("email", String(128), nullable=False)
+    password = Column("password", String(128), nullable=False)
+    first_name = Column("first_name", String(128), nullable=False)
+    last_name = Column("last_name", String(128), nullable=False)
+    # email = ""
+    # password = ""
+    # first_name = ""
+    # last_name = ""
