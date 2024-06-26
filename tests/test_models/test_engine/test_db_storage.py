823795d7a5e7cb7fb2837ef75b845471e4ef8ef3
Al-ameen Adeyemi <adeyemialameen04@gmail.com>
2024-06-26 17:21:57 +0100

    tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,183 +1,106 @@
-#!/usr/bin/python3
-''' module for file_storage tests '''
 import unittest
-import MySQLdb
-from models.user import User
 from models import storage
-from datetime import datetime
-from envs import (
-    HBNB_TYPE_STORAGE,
-    HBNB_MYSQL_DB,
-    HBNB_MYSQL_USER,
-    HBNB_MYSQL_PWD,
-    HBNB_MYSQL_HOST
-)
+from models.state import State
+from models.city import City
+from models.user import User
 
 
-@unittest.skipIf(HBNB_TYPE_STORAGE != 'db',
-                 'db_storage test not supported')
 class TestDBStorage(unittest.TestCase):
-    '''testing dbstorage engine'''
+    """Test the DBStorage class"""
 
-    def test_new_and_save(self):
-        '''testing  the new and save methods'''
-        db = MySQLdb.connect(user=HBNB_MYSQL_USER,
-                             host=HBNB_MYSQL_HOST,
-                             passwd=HBNB_MYSQL_PWD,
-                             port=3306,
-                             db=HBNB_MYSQL_DB)
-        new_user = User(**{'first_name': 'jack',
-                           'last_name': 'bond',
-                           'email': 'jack@bond.com',
-                           'password': 12345})
-        cur = db.cursor()
-        cur.execute('SELECT COUNT(*) FROM users')
-        old_count = cur.fetchall()
-        cur.close()
-        db.close()
-        new_user.save()
-        db = MySQLdb.connect(user=HBNB_MYSQL_USER,
-                             host=HBNB_MYSQL_HOST,
-                             passwd=HBNB_MYSQL_PWD,
-                             port=3306,
-                             db=HBNB_MYSQL_DB)
-        cur = db.cursor()
-        cur.execute('SELECT COUNT(*) FROM users')
-        new_count = cur.fetchall()
-        self.assertEqual(new_count[0][0], old_count[0][0] + 1)
-        cur.close()
-        db.close()
+    @classmethod
+    def setUpClass(cls):
+        """Set up for test"""
+        cls.state = State(name="California")
+        cls.city = City(name="San Francisco", state_id=cls.state.id)
+        storage.new(cls.state)
+        storage.new(cls.city)
+        storage.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Clean up after tests"""
+        storage.delete(cls.city)
+        storage.delete(cls.state)
+        storage.save()
+
+    def test_all(self):
+        """Test the all method"""
+        all_objs = storage.all()
+        self.assertIn(f'State.{self.state.id}', all_objs)
+        self.assertIn(f'City.{self.city.id}', all_objs)
 
     def test_new(self):
-        """ New object is correctly added to database """
-        new = User(
-            email='john2020@gmail.com',
-            password='password',
-            first_name='John',
-            last_name='Zoldyck'
-        )
-        self.assertFalse(new in storage.all().values())
-        new.save()
-        self.assertTrue(new in storage.all().values())
-        dbc = MySQLdb.connect(
-            host=HBNB_MYSQL_HOST,
-            port=3306,
-            user=HBNB_MYSQL_USER,
-            passwd=HBNB_MYSQL_PWD,
-            db=HBNB_MYSQL_DB
-        )
-        cursor = dbc.cursor()
-        cursor.execute('SELECT * FROM users WHERE id="{}"'.format(new.id))
-        result = cursor.fetchone()
-        self.assertTrue(result is not None)
-        self.assertIn('john2020@gmail.com', result)
-        self.assertIn('password', result)
-        self.assertIn('John', result)
-        self.assertIn('Zoldyck', result)
-        cursor.close()
-        dbc.close()
-
-    def test_delete(self):
-        """ Object is correctly deleted from database """
-        new = User(
-            email='john2020@gmail.com',
-            password='password',
-            first_name='John',
-            last_name='Zoldyck'
-        )
-        obj_key = 'User.{}'.format(new.id)
-        dbc = MySQLdb.connect(
-            host=HBNB_MYSQL_HOST,
-            port=3306,
-            user=HBNB_MYSQL_USER,
-            passwd=HBNB_MYSQL_PWD,
-            db=HBNB_MYSQL_DB
-        )
-        new.save()
-        self.assertTrue(new in storage.all().values())
-        cursor = dbc.cursor()
-        cursor.execute('SELECT * FROM users WHERE id="{}"'.format(new.id))
-        result = cursor.fetchone()
-        self.assertTrue(result is not None)
-        self.assertIn('john2020@gmail.com', result)
-        self.assertIn('password', result)
-        self.assertIn('John', result)
-        self.assertIn('Zoldyck', result)
-        self.assertIn(obj_key, storage.all(User).keys())
-        new.delete()
-        self.assertNotIn(obj_key, storage.all(User).keys())
-        cursor.close()
-        dbc.close()
-
-    def test_reload(self):
-        """ Tests the reloading of the database session """
-        dbc = MySQLdb.connect(
-            host=HBNB_MYSQL_HOST,
-            port=3306,
-            user=HBNB_MYSQL_USER,
-            passwd=HBNB_MYSQL_PWD,
-            db=HBNB_MYSQL_DB
-        )
-        cursor = dbc.cursor()
-        cursor.execute(
-            'INSERT INTO users(id, created_at, updated_at, email, password' +
-            ', first_name, last_name) VALUES(%s, %s, %s, %s, %s, %s, %s);',
-            [
-                '4447-by-me',
-                str(datetime.now()),
-                str(datetime.now()),
-                'ben_pike@yahoo.com',
-                'pass',
-                'Benjamin',
-                'Pike',
-            ]
-        )
-        self.assertNotIn('User.4447-by-me', storage.all())
-        dbc.commit()
-        storage.reload()
-        self.assertIn('User.4447-by-me', storage.all())
-        cursor.close()
-        dbc.close()
+        """Test the new method"""
+        new_state = State(name="Nevada")
+        storage.new(new_state)
+        storage.save()
+        all_objs = storage.all(State)
+        self.assertIn(f'State.{new_state.id}', all_objs)
+        storage.delete(new_state)
+        storage.save()
 
     def test_save(self):
-        """ object is successfully saved to database """
-        new = User(
-            email='john2020@gmail.com',
-            password='password',
-            first_name='John',
-            last_name='Zoldyck'
-        )
-        dbc = MySQLdb.connect(
-            host=HBNB_MYSQL_HOST,
-            port=3306,
-            user=HBNB_MYSQL_USER,
-            passwd=HBNB_MYSQL_PWD,
-            db=HBNB_MYSQL_DB
-        )
-        cursor = dbc.cursor()
-        cursor.execute('SELECT * FROM users WHERE id="{}"'.format(new.id))
-        result = cursor.fetchone()
-        cursor.execute('SELECT COUNT(*) FROM users;')
-        old_cnt = cursor.fetchone()[0]
-        self.assertTrue(result is None)
-        self.assertFalse(new in storage.all().values())
-        new.save()
-        dbc1 = MySQLdb.connect(
-            host=HBNB_MYSQL_HOST,
-            port=3306,
-            user=HBNB_MYSQL_USER,
-            passwd=HBNB_MYSQL_PWD,
-            db=HBNB_MYSQL_DB
-        )
-        cursor1 = dbc1.cursor()
-        cursor1.execute('SELECT * FROM users WHERE id="{}"'.format(new.id))
-        result = cursor1.fetchone()
-        cursor1.execute('SELECT COUNT(*) FROM users;')
-        new_cnt = cursor1.fetchone()[0]
-        self.assertFalse(result is None)
-        self.assertEqual(old_cnt + 1, new_cnt)
-        self.assertTrue(new in storage.all().values())
-        cursor1.close()
-        dbc1.close()
-        cursor.close()
-        dbc.close()
+        """Test the save method"""
+        new_state = State(name="Texas")
+        storage.new(new_state)
+        storage.save()
+        all_objs = storage.all(State)
+        self.assertIn(f'State.{new_state.id}', all_objs)
+        storage.delete(new_state)
+        storage.save()
+
+    def test_delete(self):
+        """Test the delete method"""
+        new_state = State(name="Arizona")
+        storage.new(new_state)
+        storage.save()
+        storage.delete(new_state)
+        storage.save()
+        all_objs = storage.all(State)
+        self.assertNotIn(f'State.{new_state.id}', all_objs)
+
+    # def test_reload(self):
+    #     """Test the reload method"""
+    #     storage.reload()
+    #     all_objs = storage.all()
+    #     self.assertIn(f'State.{self.state.id}', all_objs)
+    #     self.assertIn(f'City.{self.city.id}', all_objs)
+
+    def test_all_with_cls(self):
+        """Test the all method with class filter"""
+        all_states = storage.all(State)
+        all_cities = storage.all(City)
+        self.assertIn(f'State.{self.state.id}', all_states)
+        self.assertNotIn(f'City.{self.city.id}', all_states)
+        self.assertIn(f'City.{self.city.id}', all_cities)
+
+    def test_state_count(self):
+        """Test the count of states before and after adding a new state"""
+        initial_count = len(storage.all(State))
+        new_state = State(name="Washington")
+        storage.new(new_state)
+        storage.save()
+        new_count = len(storage.all(State))
+        self.assertEqual(new_count, initial_count + 1)
+        storage.delete(new_state)
+        storage.save()
+        final_count = len(storage.all(State))
+        self.assertEqual(final_count, initial_count)
+
+    def test_user_creation(self):
+        """Test user creation"""
+        initial_count = len(storage.all(User))
+        new_user = User(email="newuser@example.com", password="newpassword")
+        storage.new(new_user)
+        storage.save()
+        new_count = len(storage.all(User))
+        self.assertEqual(new_count, initial_count + 1)
+        storage.delete(new_user)
+        storage.save()
+        final_count = len(storage.all(User))
+        self.assertEqual(final_count, initial_count)
+
+
+if __name__ == '__main__':
+    unittest.main()
